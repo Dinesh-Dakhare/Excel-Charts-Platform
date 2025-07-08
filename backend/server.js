3d121749231b06182a2da10e981d39db1403017d
Dinesh Dakhare <dineshdakhare22@gmail.com>
2025-07-08 11:26:04 +0530

    vite , tailwinccss config

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+import express from 'express'
+const app = express()
+const port = 3000
 
-dotenv.config();
-const app = express();
-connectDB();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", require("./routes/authRoutes"));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
